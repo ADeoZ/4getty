1172c6873f41b6460627433701db00c177a64346
Aleksey Denisov <deoz@yandex.ru>
2023-07-18 22:12:16 +0300

    DEV / classname props for A

--- components/basic/A/index.tsx
+++ components/basic/A/index.tsx
@@ -1,5 +1,5 @@
 import Link, { LinkProps } from 'next/link';
 
-type AProps = LinkProps & { children?: React.ReactNode };
+type AProps = LinkProps & { className?: string; children?: React.ReactNode };
 
 export const A = ({ children, ...props }: AProps) => <Link {...props}>{children}</Link>;
