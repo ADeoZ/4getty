9bcaf65b7c75c17bfa0886352a7dae424fe322bf
Aleksey Denisov <deoz@yandex.ru>
2023-07-18 22:31:28 +0300

    DEV / IndexLogo & IndexMenu

---
+++ components/widgets/IndexLogo/index.tsx
@@ -0,0 +1,3 @@
+import { Logo } from '@/entities/Logo';
+
+export const IndexLogo = () => <Logo className='mb-0 text-[10rem]' />;
---
+++ components/widgets/IndexMenu/index.tsx
@@ -0,0 +1,3 @@
+import { Menu } from '@/entities/Menu';
+
+export const IndexMenu = () => <Menu className='gap-6 text-xl' />;
