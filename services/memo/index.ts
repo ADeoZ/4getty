772a1479493b2cad861303e179d9c14402d0cc84
Aleksey Denisov <deoz@yandex.ru>
2023-08-07 22:40:05 +0300

    DEV / get memo api

--- services/memo/index.ts
+++ services/memo/index.ts
@@ -1,11 +1,20 @@
 export class apiMemo {
-  static async makeMark({ memoId }: { memoId: number }): Promise<{ success: string }> {
-    const res = await fetch(`/api/memo/${memoId}/mark`, {
+  static async getMemo({ memoId }: { memoId: number }) {
+    const res = await fetch(`${process.env.BASE_URL ?? ''}/api/memo/${memoId}`, {
+      method: 'GET',
+    });
+
+    return res.json() as Promise<{
+      title: string;
+      content: string;
+      rating: number;
+      hasMark: boolean;
+    }>; // fix
+  }
+
+  static async makeMark({ memoId }: { memoId: number }) {
+    const res = await fetch(`${process.env.BASE_URL ?? ''}/api/memo/${memoId}/mark`, {
       method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
     });
 
     return res.json() as Promise<{ success: string }>; // fix
