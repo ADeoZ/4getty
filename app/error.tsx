a8966f8835c5cb7aa3841c458570437fc8f6ab71
Aleksey Denisov <deoz@yandex.ru>
2023-08-30 22:33:54 +0300

    DEV / Show error message

--- app/error.tsx
+++ app/error.tsx
@@ -1,26 +1,33 @@
 'use client';
 
+import { useEffect } from 'react';
 import { A } from '@/basic/A';
 import { H2 } from '@/basic/typography/H2';
 import { P } from '@/basic/typography/P';
 import { Card } from '@/entities/Card';
 
-type ErrorProps = { reset: () => void };
+type ErrorProps = { error: Error & { digest?: string }; reset: () => void };
 
-const Error = ({ reset }: ErrorProps) => (
-  <div className='flex h-screen items-center justify-center'>
-    <Card>
-      <H2>Что-то пошло не так...</H2>
-      <P>
-        Но это исключительный случай!
-        <br />
-        Может поможет{' '}
-        <A href='' onClick={() => reset()} className='underline'>
-          перезагрузка
-        </A>
-        ?
-      </P>
-    </Card>
-  </div>
-);
+const Error = ({ error, reset }: ErrorProps) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <Card>
+        <H2>{error.message || 'Что-то пошло не так...'}</H2>
+        <P>
+          Но это исключительный случай!
+          <br />
+          Может поможет{' '}
+          <A href='' onClick={() => reset()} className='underline'>
+            перезагрузка
+          </A>
+          ?
+        </P>
+      </Card>
+    </div>
+  );
+};
 export default Error;
