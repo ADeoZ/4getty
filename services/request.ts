532e936b11f20f2445b225c1821fd30caaac4b01
Aleksey Denisov <deoz@yandex.ru>
2023-08-30 22:34:07 +0300

    DEV / Request error handling

--- services/request.ts
+++ services/request.ts
@@ -6,14 +6,38 @@ export class Request {
       method: 'GET',
     });
 
+    this.errorHandler(result);
+
     return result.json() as Promise<T>;
   }
 
-  static async post<T>(url: string) {
+  static async post<T>(url: string, body?: BodyInit) {
     const result = await fetch(`${this.host}${url}`, {
       method: 'POST',
+      body,
     });
 
+    this.errorHandler(result);
+
     return result.json() as Promise<T>;
+  }
+
+  static errorHandler(serverResponse: Response): void {
+    if (!serverResponse.ok) {
+      switch (serverResponse.status) {
+        case 400:
+          throw new Error('Ошибка в запросе');
+        case 401:
+          throw new Error('Ошибка авторизации');
+        case 403:
+          throw new Error('Доступ ограничен');
+        case 404:
+          throw new Error('Запрошенный объект не доступен');
+        case 500:
+          throw new Error('Серверная ошибка');
+        default:
+          throw new Error('Ошибка');
+      }
+    }
   }
 }
