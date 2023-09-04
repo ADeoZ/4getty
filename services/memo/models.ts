fe71395858da3c40c4798913e605b2b8fe85e715
Aleksey Denisov <deoz@yandex.ru>
2023-09-04 22:57:19 +0300

    DEV / memo models extended

--- services/memo/models.ts
+++ services/memo/models.ts
@@ -1,9 +1,36 @@
+export type MemoTypes = 'todo' | 'list';
+
+type ToDoItem = {
+  id: string;
+  label: string;
+  checked: boolean;
+};
+
+type MemoTimestamps = {
+  created: string;
+  updated: string;
+};
+
+type MemoCategory = {
+  id: string;
+  name: string;
+};
+
+type MemoRating = { value: number; hasMark: boolean };
+
+type MemoTag = string;
+
 export type MemoItem = {
   id: number;
+  author: string;
+  timestamps: MemoTimestamps;
   title: string;
+  type: MemoTypes;
+  category: MemoCategory;
+  tags: MemoTag[];
+  list: ToDoItem[];
   content: string;
-  rating: number;
-  hasMark: boolean;
+  rating: MemoRating;
 };
 
 export type ListMemo = MemoItem[];
