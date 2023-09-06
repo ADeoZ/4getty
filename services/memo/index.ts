f4c6a1294464ce249f0618a348382c4a88dc9327
Aleksey Denisov <deoz@yandex.ru>
2023-09-06 23:09:56 +0300

    DEV / check item requests

--- services/memo/index.ts
+++ services/memo/index.ts
@@ -1,5 +1,10 @@
 import { Request } from 'services/request';
-import { MakeMarkResponse, MemoItem } from './models';
+import { MemoItem, ToDoItem } from './models';
+
+type SuccessResponse = { success: string };
+
+type CheckItemRequest = { memoId: MemoItem['id']; itemId: ToDoItem['id'] };
+type UncheckItemRequest = CheckItemRequest;
 
 export class ApiMemo {
   static async getMemo({ id }: Pick<MemoItem, 'id'>) {
@@ -7,6 +12,14 @@ export class ApiMemo {
   }
 
   static async makeMark({ id }: Pick<MemoItem, 'id'>) {
-    return Request.post<MakeMarkResponse>(`/api/memo/${id}/mark`);
+    return Request.post<SuccessResponse>(`/api/memo/${id}/mark`);
+  }
+
+  static async checkItem({ memoId, itemId }: CheckItemRequest) {
+    return Request.post<SuccessResponse>(`/api/memo/${memoId}/item/${itemId}/check`);
+  }
+
+  static async uncheckItem({ memoId, itemId }: UncheckItemRequest) {
+    return Request.post<SuccessResponse>(`/api/memo/${memoId}/item/${itemId}/uncheck`);
   }
 }
