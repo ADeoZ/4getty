a648189a1f26e1067d595e57775b28b67c9fa27c
Aleksey Denisov <deoz@yandex.ru>
2023-09-13 23:00:34 +0300

    FIX / card header category links

--- components/entities/Category/index.tsx
+++ components/entities/Category/index.tsx
@@ -17,11 +17,11 @@ const categoryLabels: CategoryLabels = {
 
 export const Category = ({ category, type }: CategoryProps) => (
   <div className='flex gap-2'>
-    <A href='#category' className='font-semibold hover:text-cyan-600'>
+    <A href={`/categories/${category.id}`} className='font-semibold hover:text-cyan-600'>
       {category.name}
     </A>
     &#8594;
-    <A href='#type' className='font-semibold hover:text-cyan-600'>
+    <A href={`/categories/${category.id}/${type}`} className='font-semibold hover:text-cyan-600'>
       {categoryLabels[type]}
     </A>
   </div>
