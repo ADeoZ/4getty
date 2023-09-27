5a384c8b3dd48e2875bd057f15fa8f4cbff278d6
Aleksey Denisov <deoz@yandex.ru>
2023-09-27 22:31:24 +0300

    DEV / add ArrowRightIcon in Category

--- components/entities/Category/index.tsx
+++ components/entities/Category/index.tsx
@@ -1,5 +1,6 @@
 import { MemoCategory, MemoTypes } from 'services/memo/models';
 import { A } from '@/basic/A';
+import { ArrowRightIcon } from '@/basic/icons/ArrowRightIcon';
 
 type CategoryProps = {
   category: MemoCategory;
@@ -20,7 +21,7 @@ export const Category = ({ category, type }: CategoryProps) => (
     <A href={`/categories/${category.id}`} className='font-semibold hover:text-cyan-600'>
       {category.name}
     </A>
-    &#8594;
+    <ArrowRightIcon className='m-auto' />
     <A href={`/categories/${category.id}/${type}`} className='font-semibold hover:text-cyan-600'>
       {categoryLabels[type]}
     </A>
