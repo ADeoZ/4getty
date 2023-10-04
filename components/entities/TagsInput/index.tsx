2e72dd8593e51ffdc2fd26fb5a8bf3eb7c7aaeb4
Aleksey Denisov <deoz@yandex.ru>
2023-10-04 23:19:32 +0300

    DEV / TagsInput output tags

---
+++ components/entities/TagsInput/index.tsx
@@ -0,0 +1,30 @@
+import { MemoItem } from 'services/memo/models';
+import { XIcon } from '@/basic/icons/XIcon';
+import { InputLine } from '@/basic/InputLine';
+import { PlusIcon } from '@/basic/icons/PlusIcon';
+
+type TagsInputProps = Pick<MemoItem, 'tags'>;
+
+export const TagsInput = ({ tags }: TagsInputProps) => (
+  <ul className='flex flex-wrap gap-2'>
+    {tags.map((tag) => (
+      <li
+        key={tag}
+        className='flex items-center border border-zinc-200 py-0.5 pl-1.5 pr-1 text-sm text-slate-500 hover:border-cyan-600'
+      >
+        {tag}
+        <div className='ml-0.5 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full hover:bg-orange-200 focus:outline-orange-600'>
+          <XIcon />
+        </div>
+      </li>
+    ))}
+    <div className='flex min-w-[25%] grow'>
+      <PlusIcon className='self-center' />
+      <InputLine
+        initialValue='Добавить тэг'
+        className='my-px flex w-full items-center text-sm text-slate-500 empty:leading-7'
+        // submitHandler={submitHandler}
+      />
+    </div>
+  </ul>
+);
