16e78180b93efe9a781cac1fc776a2a42ce0114c
Aleksey Denisov <deoz@yandex.ru>
2023-10-16 22:14:53 +0300

    DEV / NewItemInput add ref

--- components/entities/NewItemInput/index.tsx
+++ components/entities/NewItemInput/index.tsx
@@ -1,61 +1,40 @@
-import { useState } from 'react';
+import { ElementRef, useRef } from 'react';
 import { PlusIcon } from '@/basic/icons/PlusIcon';
 import { InputLine } from '@/basic/InputLine';
 
 type NewItemInputProps = { submitHandler: (value: string) => void };
 
 export const NewItemInput = ({ submitHandler }: NewItemInputProps) => {
-  const [editing, setEditing] = useState(false);
+  const inputRef = useRef<ElementRef<typeof InputLine>>(null);
 
-  const showInputLine = () => setEditing(true);
+  const focusInputLine = () => inputRef.current?.focus();
 
   const keyHandler = (event: React.KeyboardEvent<HTMLElement>) =>
-    event.key === 'Enter' && showInputLine();
+    event.key === 'Enter' && focusInputLine();
 
   const addItemHandler = (value: string) => {
+    if (!value) return;
     submitHandler(value);
-    setEditing(false);
-  };
-
-  const resetOnEmptyValue = (value: string) => {
-    if (!value) {
-      setEditing(false);
-
-      return false;
-    }
-
-    return true;
+    inputRef.current?.reset();
   };
 
   return (
     <li className='relative py-1 pl-6'>
       <div
-        className='peer/new-item absolute left-0 top-1/2 -translate-y-1/2 cursor-pointer'
-        onClick={showInputLine}
-        onKeyDown={keyHandler}
+        className='peer/input-line absolute left-0 top-1/2 -translate-y-1/2 cursor-pointer'
+        onClick={focusInputLine}
+        onKeyUp={keyHandler}
         role='button'
         tabIndex={0}
       >
         <PlusIcon />
       </div>
-      {editing ? (
-        <InputLine
-          autoFocus
-          submitHandler={addItemHandler}
-          validateHandler={resetOnEmptyValue}
-          className='pr-10'
-        />
-      ) : (
-        <span
-          className='cursor-pointer text-slate-400 hover:text-black peer-hover/new-item:text-black'
-          onClick={showInputLine}
-          onKeyDown={keyHandler}
-          role='button'
-          tabIndex={0}
-        >
-          Добавить пункт
-        </span>
-      )}
+      <InputLine
+        placeholder='Добавить пункт'
+        submitHandler={addItemHandler}
+        className='cursor-text pr-10'
+        ref={inputRef}
+      />
     </li>
   );
 };
