7319bd6115a0eae618a49596eb3de92e508ecf85
Aleksey Denisov <deoz@yandex.ru>
2023-11-13 22:58:55 +0300

    DEV / Select callback handler

--- components/basic/Select/index.tsx
+++ components/basic/Select/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { ChevronDownIcon } from '../icons/ChevronDownIcon';
 import { ChevronUpIcon } from '../icons/ChevronUpIcon';
@@ -12,11 +12,13 @@ type SelectProps<T extends string = string> = {
   options: SelectOption<T>[];
   initialValue?: T;
   selectHandler?: (value: T) => void;
-  placeholder?: T;
   className?: string;
 };
 
-export const Select = ({ options, initialValue = '', className }: SelectProps) => {
+// TODO:
+// CategoryInput + api, placeholder, focus keydown/keyup, close on click outside, close on Esc
+
+export const Select = ({ options, initialValue = '', selectHandler, className }: SelectProps) => {
   const [value, setValue] = useState<(typeof options)[number]['value']>(initialValue);
   const [open, setOpen] = useState<boolean>(false);
 
@@ -28,6 +30,24 @@ export const Select = ({ options, initialValue = '', className }: SelectProps) =
       clickHandler();
     }
   };
+
+  const selectValueHandler = useCallback(
+    (selectedValue: (typeof options)[number]['value']) => {
+      setValue(selectedValue);
+      if (selectHandler) selectHandler(selectedValue);
+    },
+    [selectHandler],
+  );
+
+  const keySelectValueHandler = useCallback(
+    (event: React.KeyboardEvent<HTMLElement>, selectedValue: (typeof options)[number]['value']) => {
+      if (event.key === 'Enter') {
+        event?.preventDefault();
+        selectValueHandler(selectedValue);
+      }
+    },
+    [selectValueHandler],
+  );
 
   const currentLabel = options.find((option) => option.value === value)?.label;
 
@@ -46,10 +66,17 @@ export const Select = ({ options, initialValue = '', className }: SelectProps) =
         {currentLabel}
         {open ? <ChevronUpIcon /> : <ChevronDownIcon />}
         {open && (
-          <div className='absolute left-0 right-0 top-8 z-10 min-w-max border border-cyan-600 bg-slate-50/70 shadow-[0_24px_64px] shadow-zinc-800/10 backdrop-blur-sm'>
-            <ul className='flex max-h-80 max-w-xs select-none flex-col overflow-y-auto'>
+          <div className='absolute left-0 right-0 top-8 z-10 min-w-max rounded-sm border border-cyan-600 bg-slate-50/70 shadow-[0_24px_64px] shadow-zinc-800/10 backdrop-blur-sm'>
+            <ul className='scrollbar-thin flex max-h-80 max-w-xs select-none flex-col overflow-y-auto'>
               {options.map((option) => (
-                <li className='flex-shrink-0 truncate px-1.5 py-0.5 hover:bg-cyan-600/10'>
+                <li
+                  className='flex-shrink-0 truncate px-1.5 py-0.5 hover:bg-cyan-600/10'
+                  onClick={() => selectValueHandler(option.value)}
+                  onKeyDown={(event) => keySelectValueHandler(event, option.value)}
+                  role='option'
+                  aria-selected='false'
+                  key={option.value}
+                >
                   {option.label}
                 </li>
               ))}
