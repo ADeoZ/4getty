245bc0f87ecfb60f92f13c7d03aa11e17326aeb5
Aleksey Denisov <deoz@yandex.ru>
2024-05-16 12:55:28 +0300

    FIX / low post mark delay

--- app/api/memo/[memoId]/mark/route.ts
+++ app/api/memo/[memoId]/mark/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function POST() {
   const data = await new Promise((resolve) => {
-    setTimeout(() => resolve({ success: true }), 3000);
+    setTimeout(() => resolve({ success: true }), 200);
   });
 
   return NextResponse.json(data);
